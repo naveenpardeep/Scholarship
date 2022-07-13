3613b32666064010a7ddac7f87209667870c7f11
naveenpardeep <naveensheoran1000@gmail.com>
2022-07-13 22:50:47 +0300

    ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:scholarship/view/listview.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,6 +7,12 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // ignore: unused_element
+  Future<InitializationStatus> _initGoogleMobileAds() {
+    // TODO: Initialize Google Mobile Ads SDK
+    return MobileAds.instance.initialize();
+  }
+
   // ignore: prefer_const_constructors
   runApp(MaterialApp(
     initialRoute: '/',
--- lib/view/listview.dart
+++ lib/view/listview.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:scholarship/main.dart';
 import 'package:scholarship/view/viewpage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:scholarship/ads/adshelper.dart';
+
 
 class Listview extends StatefulWidget {
   Listview({Key? key}) : super(key: key);
@@ -13,10 +16,39 @@ class Listview extends StatefulWidget {
 }
 
 class _ListviewState extends State<Listview> {
+
+  // TODO: Add _bannerAd
+  BannerAd? _bannerAd;
+
   final db = FirebaseFirestore.instance;
   List<Object> scholarship = [];
 
   final ScrollController controller = ScrollController();
+@override
+void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  // TODO: Load a banner ad
+  BannerAd(
+    adUnitId: AdHelper.bannerAdUnitId,
+    request: const AdRequest(),
+    size: AdSize.banner,
+    listener: BannerAdListener(
+      onAdLoaded: (ad) {
+        setState(() {
+          _bannerAd = ad as BannerAd;
+        });
+      },
+      onAdFailedToLoad: (ad, err) {
+        print('Failed to load a banner ad: ${err.message}');
+        ad.dispose();
+      },
+    ),
+  ).load();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
@@ -35,29 +67,58 @@ class _ListviewState extends State<Listview> {
                 );
               } else {
                 return SafeArea(
-                    child: Scrollbar(
+                child:  Stack
+                (children: [
+                  if (_bannerAd != null) 
+                             Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: _bannerAd!.size.width.toDouble(),
+                height: _bannerAd!.size.height.toDouble(),
+                child: AdWidget(ad: _bannerAd!),
+              ),
+            )
+                           
+                           ,
+                     Scrollbar(
                         controller: controller,
-                        child: ListView(
+                        child: 
+                          
+                           ListView(
                           children: snapshot.data!.docs.map((doc) {
-                            return Card(
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60)),
-                              child: ListTile(
-                                  textColor: Colors.black,
-                                  leading: Icon(Icons.school),
-                                  trailing: Icon(Icons.forward),
-                                  title: Text(doc['country']),
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => Viewpage(
-                                                Country: doc['country'],
-                                                Details: doc['details'])));
-                                  }),
-                            );
+                           
+
+                          return Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60)),
+                            child: ListTile(
+                                textColor: Colors.black,
+                                leading: Icon(Icons.school),
+                                trailing: Icon(Icons.forward),
+                                title: Text(doc['country']),
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Viewpage(
+                                              Country: doc['country'],
+                                              Details: doc['details'])));
+                                }),
+                          );
                           }).toList(),
-                        )));
+                        ))
+                        ]
+                ));
               }
             }));
   }
+  @override
+void dispose() {
+  // TODO: Dispose a BannerAd object
+  _bannerAd?.dispose();
+
+  
+
+  super.dispose();
+}
+
 }
