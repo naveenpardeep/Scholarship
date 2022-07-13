38ef5ba72e5ff257378723311269b231f1c1dee6
naveenpardeep <naveensheoran1000@gmail.com>
2022-07-13 22:27:30 +0300

    admob

--- lib/view/viewpage.dart
+++ lib/view/viewpage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 
 class Viewpage extends StatefulWidget {
   var Country;
@@ -17,6 +19,9 @@ class _ViewpageState extends State<Viewpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.Country),
+      backgroundColor: Color.fromARGB(44, 15, 14, 71)),
         body: SafeArea(
       child: Center(child: Text(widget.Details)),
     ));
